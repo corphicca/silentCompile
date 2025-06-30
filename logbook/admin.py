acf786f2c219aaacd7db2ef587552df1d00866dc
Cidayin Nahuat <cidayinn@gmail.com>
2025-06-30 09:41:54 -0600

    added all classes to admin to test

--- logbook/admin.py
+++ logbook/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import ProgrammerMilestone, EntryTag
+from .models import Entry, Tag, Programmer, Milestone, ProgrammerMilestone, EntryTag
 
 # Register your models here.
-admin.site.register(ProgrammerMilestone)
-admin.site.register(EntryTag)
+admin.site.register(ProgrammerMilestone) #Link Programmer and Milestone along with achieved date
+admin.site.register(EntryTag) #Link Entry and Tag 
+admin.site.register(Entry)
+admin.site.register(Tag)
+admin.site.register(Programmer)
+admin.site.register(Milestone)
